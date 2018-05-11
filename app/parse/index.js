ab2ed40641ee9b525195ee4a53343c93a5c0ca66
严俊东 <1172482914@qq.com>
2018-05-12 00:57:17 +0800

    获取文件头部

--- app/parse/index.js
+++ app/parse/index.js
@@ -3,10 +3,27 @@ const fs = require('fs')
 
 const postDir = path.resolve(__dirname, '../../post')
 const jsonDir = path.resolve(__dirname, '../../json')
-const dataPath = {fileName: jsonDir + '/files.json'}
+const dataPath = {list: jsonDir + '/list.json'}
 
 {
     // 存储目录
     let mdFile = fs.readdirSync(postDir)
-    fs.writeFileSync(dataPath.fileName, JSON.stringify(mdFile))
+    mdFile.filter(item => path.extname(item) == '.md') // 过滤非md文件
+    fs.writeFileSync(dataPath.list, JSON.stringify(mdFile))
+}
+
+{
+    // 存储详情
+    let mdFile = fs.readFileSync(dataPath.list).toString()
+    mdFile = JSON.parse(mdFile)
+    mdFile = mdFile.map(item => {
+        if (typeof item == 'string') { // 文件名转对象
+            let post = fs.readFileSync(path.join(postDir, item)).toString()
+            let test = /---[\s\S]*?(?=---)/.exec(post)
+            console.log(test[0])
+        }
+        // return item
+    })
+    // console.log(mdFile)
+    // fs.writeFileSync(dataPath.list, JSON.stringify(mdFile))
 }

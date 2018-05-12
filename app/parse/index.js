af4fdfe2850593bc8be93bee32519016a543a9f6
严俊东 <1172482914@qq.com>
2018-05-13 01:06:14 +0800

    解析md文件（列表json、详情json）

--- app/parse/index.js
+++ app/parse/index.js
@@ -1,29 +1,44 @@
 const path = require('path')
 const fs = require('fs')
-
+// const meta = require('meta')
+const yaml = require('js-yaml')
 const postDir = path.resolve(__dirname, '../../post')
 const jsonDir = path.resolve(__dirname, '../../json')
-const dataPath = {list: jsonDir + '/list.json'}
+const dataPath = { list: path.join(jsonDir, 'list'), post: path.join(jsonDir, 'post') }
 
+if (!fs.existsSync(jsonDir)) fs.mkdirSync(jsonDir) // 如果json目录不存在则创建
+Object.keys(dataPath).forEach(key => {
+    if (!fs.existsSync(dataPath[key])) fs.mkdirSync(dataPath[key]) 
+})
 {
-    // 存储目录
+    // 获取post文件名
     let mdFile = fs.readdirSync(postDir)
-    mdFile.filter(item => path.extname(item) == '.md') // 过滤非md文件
-    fs.writeFileSync(dataPath.list, JSON.stringify(mdFile))
-}
-
-{
-    // 存储详情
-    let mdFile = fs.readFileSync(dataPath.list).toString()
-    mdFile = JSON.parse(mdFile)
+    mdFile = mdFile.filter(item => path.extname(item) == '.md') // 过滤非md文件
     mdFile = mdFile.map(item => {
         if (typeof item == 'string') { // 文件名转对象
+            const basename = path.basename(item, '.md')
             let post = fs.readFileSync(path.join(postDir, item)).toString()
-            let test = /---[\s\S]*?(?=---)/.exec(post)
-            console.log(test[0])
+            let bloks = post.split('---')
+            let metaYaml = bloks[1]
+            let postContent = bloks.splice(2).join('') // 文章主体内容
+            let meta = yaml.load(metaYaml) // 文章媒体信息
+            meta.basename = basename // 如果没有id，默认为文件名
+            let detail = { meta, content: postContent }
+            fs.writeFileSync(path.join(dataPath.post, basename + '.json'), JSON.stringify(detail))
+            return meta
+        } else {
+            return item
         }
-        // return item
     })
-    // console.log(mdFile)
-    // fs.writeFileSync(dataPath.list, JSON.stringify(mdFile))
+    // 存储分页信息
+    const totalNum = mdFile.length
+    let page = 0
+    const pageSize = 10
+    while (mdFile.length) {
+        page++
+        let list = mdFile.splice(0, pageSize)
+        let listObj = {totalNum, page, pageSize, list}
+        fs.writeFileSync(path.join(dataPath.list, page + '.json'), JSON.stringify(listObj))
+    }
+    // 列表存储
 }

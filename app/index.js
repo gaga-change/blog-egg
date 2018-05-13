3b1eb13c8d764273b01bf32f155aa50d3dbe8ebf
严俊东 <1172482914@qq.com>
2018-05-13 14:56:39 +0800

    静态资源导入

--- app/index.js
+++ app/index.js
@@ -2,30 +2,34 @@
 const Koa = require('koa')
 const logger = require('koa-logger')
 const router = require('koa-router')()
-const koaBody = require('koa-body')
 const views = require('koa-views')
+const koaBody = require('koa-body')
 const path = require('path')
+const staticCache = require('koa-static-cache')
+
 const parse = require('./parse')
 
 parse() // 解析post目录下的md文件
 
 const app = module.exports = new Koa()
 
+app.use(koaBody())
 app.use(logger())
-
-app.use(views(path.join(__dirname, '../src'), {
+app.use(staticCache(path.resolve(__dirname, '../publish'), {
+    maxAge: 365 * 24 * 60 * 60,
+    gzip: true,
+}))
+app.use(views(path.resolve(__dirname, '../page'), {
     map: { html: 'ejs' }
 }))
 
-app.use(koaBody)
 router.get('/', home)
 app.use(router.routes())
 
 async function home(ctx) {
-    console.log('---')
     await ctx.render('index')
 }
 
-if (!module.parent) app.listen(3000, () => {
-    console.log(3000)
+if (!module.parent) app.listen(8080, () => {
+    console.log(8080)
 })

bc9996ef31f13d334b18f6bc4f29565458189f55
严俊东 <1172482914@qq.com>
2018-05-20 23:12:13 +0800

    mongodb 配置文件

---
+++ app/config/mongo.js
@@ -0,0 +1,16 @@
+const mongodbUsername = process.env.MONGODB_USERNAME || ''
+const mongodbPassword = process.env.MONGODB_PASSWORD || ''
+const mongodbHost = process.env.MONGODB_HOST || 'localhost'
+const mongodbPort = process.env.MONGODB_PORT || '27017'
+const mongodbCollection = process.env.MONGODB_COLLECTION || 'test'
+
+let mongodbConnectLink = ''
+
+let userpwd = ''
+if (mongodbUsername) {
+    userpwd = `${mongodbUsername}:${mongodbPassword}@`
+}
+
+mongodbConnectLink = `mongodb://${userpwd}${mongodbHost}:${mongodbPort}/${mongodbCollection}`
+
+exports.link = mongodbConnectLink

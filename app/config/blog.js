7c3260b24e28b3c9cb5f2be2e55036e1cfd1fa33
严俊东 <1172482914@qq.com>
2018-05-27 21:43:21 +0800

    版本可配置

--- app/config/blog.js
+++ app/config/blog.js
@@ -6,7 +6,7 @@ const config = {
     "subhead": "严俊东个人博客",
     "description": "严俊东的个人博客。技术包括但不限于JavaScript、NodeJS、CSS3、HTML以及各类编程开发等相关内容。邮箱gaga_change@qq.com，微信号gaga_change。",
     "keywords": "严俊东,严俊东个人博客,严俊东博客,喜欢JS的无名小站",
-    "version": "201805212203"
+    "version": process.env.MY_BLOG_STATIC_VERSION || "3.1.0"
 }
 
 module.exports = config

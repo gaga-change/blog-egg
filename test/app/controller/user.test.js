312eacbff2453e0dcdc42586a43a9eb4f467de2f
严俊东 <1172482914@qq.com>
2019-09-25 14:41:00 +0800

    /api/users 单元测试

---
+++ test/app/controller/user.test.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const { app, assert } = require('egg-mock/bootstrap');
+
+describe('test/app/controller/user.test.js', () => {
+
+  it('should GET /api/users', () => {
+    return app.httpRequest()
+      .get('/api/users')
+      .expect(200)
+      .then(response => {
+        assert(response.body.constructor === Array )
+    })
+  });
+});

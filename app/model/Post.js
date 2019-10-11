fb41d950798697a30408125e4103a0cf8abaac35
严俊东 <1172482914@qq.com>
2019-10-11 17:10:35 +0800

    笔记目录只能绑定一个、笔记添加状态&历史版本

--- app/model/Post.js
+++ app/model/Post.js
@@ -11,10 +11,11 @@ module.exports = app => {
     content: { type: String, default: '', trim: true }, // 主要内容
     title: { type: String, default: '', trim: true }, // 标题
     tags: { type: Array, default: [] }, // 标签
-    categories: { type: Array, default: [] }, // 目录
+    category: { type: Schema.Types.ObjectId, ref: 'Category' }, // 目录
     id: { type: Number, default: 0 }, // 文档ID值
     date: { type: Date, default: Date.now }, // 创建时间
-    // state: { type: Number, default: TYPE.PUBLISH }, // 状态 [发布,草稿,私密,移除]
+    type: { type: Number, default: 0 }, // 状态 [草稿：0，发布：1，历史：2， 废弃：3]
+    history: { type: Array, default: [] }, // 历史版本
     intro: { type: String, default: '', trim: true }, // 描述
     markdown: { type: String, default: '' }, // 源文件内容
   }, {

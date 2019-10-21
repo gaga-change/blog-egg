f875afa60db4269ffde6ca290cff3f43504768ea
严俊东 <1172482914@qq.com>
2019-10-21 17:04:16 +0800

    展示页菜单 文档结构

---
+++ app/model/PageMenu.js
@@ -0,0 +1,22 @@
+'use strict';
+
+/**
+ * @param {Egg.Application} app - egg application
+ */
+module.exports = app => {
+  const mongoose = app.mongoose;
+  const Schema = mongoose.Schema;
+
+  const PageMenuSchema = new Schema({
+    path: { type: String, default: '', trim: true }, // 路径
+    name: { type: String, default: '', trim: true }, // 名称
+    sons: [{ type: Schema.Types.ObjectId, ref: 'PageMenu' }], // 子菜单列表
+    type: { type: Number, default: 0 }, // 类型： 0是内链，1是外链
+    show: { type: Boolean, default: true }, // 是否显示
+    index: { type: Number, default: 0, trim: true }, // 优先级
+  }, {
+    timestamps: true,
+  });
+
+  return mongoose.model('PageMenu', PageMenuSchema, 'blog_page_menu');
+};
